d8802d33fa986938823b99f42d45eaac5f15794b
dawid <dawidpisarski96@gmail.com>
2022-11-27 15:04:44 +0100

    grazynka

---
+++ js.js
@@ -0,0 +1,28 @@
+const pas = document.querySelector(".inputPassword");
+const inputValue = document.getElementById("password").value;
+const correct = document.querySelector(".correct");
+const unCorrect = document.querySelector(".un_correct");
+const btn = document.querySelector(".btn");
+
+const showMsg = () => {
+  content = pas.value;
+
+  if (content == "macgyver 2k22") {
+    correct.classList.add("active");
+    unCorrect.classList.remove("unactive");
+    console.log("działa");
+  } else {
+    correct.classList.remove("active");
+    unCorrect.classList.add("unactive");
+    console.log("nie dziala");
+  }
+};
+
+const enterKeyCheck = (e) => {
+  if (e.key === "Enter") {
+    showMsg();
+  }
+};
+
+btn.addEventListener("click", showMsg);
+pas.addEventListener("keyup", enterKeyCheck);
